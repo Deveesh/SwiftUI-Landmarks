8d7f0bb477cf591367efd5e61e16664c00ed7dec
Deveesh <deveesh97@gmail.com>
2024-09-02 13:10:48 +0530

    Initial Commit

---
+++ SwiftUI-Landmarks/SwiftUI_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LandmarksApp.swift
+//  SwiftUI-Landmarks
+//
+//  Created by Deveesh Mehandiratta on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
